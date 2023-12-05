876220169ae11ce0669eed17bfc930456fef2240
Feben9 <117801933+Feben9@users.noreply.github.com>
2023-12-05 22:56:19 +0300

    Updated js file to include forecast section when page loads for the first time

--- src/index.js
+++ src/index.js
@@ -119,3 +119,4 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearch);
 
 searchCity("Addis Ababa");
+getForecast("Addis Ababa");
